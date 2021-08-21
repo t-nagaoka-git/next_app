9615fb0c56e49a3c48068def59c8ea3a62e18e6b
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-21 21:53:07 +0900

    323 data.txt のtextを取得する 疑問:fetch の引数は URL 以外でもよいのか？

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,14 @@ import Layout from '../components/layout';
 import useSWR from 'swr';
 
 export default function Home() {
-  const {data} = useSWR('/data.json');
+  const func = (...args) => fetch(...args).then((res) => res.text());
+  const {data, err} = useSWR('/data.txt', func);
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data != undefined ? data.message : 'error...'}</h5>
-          <table className="table table-dark">
-            <thead className="">
-              <tr>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data != undefined ? (
-                data.data.map((value, key) => (
-                  <tr key={key}>
-                    <td>{value.name}</td>
-                    <td>{value.mail}</td>
-                    <td>{value.age}</td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td></td>
-                  <td>no data.</td>
-                  <td></td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+          <h5 className="mb-4">{data}</h5>
         </div>
       </Layout>
     </div>
