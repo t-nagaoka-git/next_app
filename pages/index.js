b01dff118113b400acf2f90c42927e7ab591fce2
Tatsuki Nagaoka <nagaoka@mbp.local>
2021-08-21 12:41:00 +0900

    294 スタイルを適用する

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,26 @@ export default function Home() {
   let title = 'Next.js page';
   let message = 'React Next.js sample page.';
 
+  const h1 = {
+    textAlign: 'right',
+    padding: '5px 15px',
+  };
+
+  const p = {
+    textAlign: 'left',
+    margin: '0px 5px',
+    color: '#669',
+    fontSize: '18pt',
+  };
+
+  const subtitle = {
+    textAlign: 'center',
+    margin: '0px 5px',
+    color: '#99d',
+    fontSize: '24pt',
+    fontWeight: 'bold',
+  };
+
   return (
     <div>
       <Head>
@@ -16,11 +36,17 @@ export default function Home() {
         ></link>
       </Head>
 
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white display-4" style={h1}>
+        React
+      </h1>
       <div className="container">
-        <h4 className="my-3">{title}</h4>
+        <p className="my-3" style={subtitle} id="subtitle">
+          {title}
+        </p>
         <div className="alert alert-primary text-center">
-          <p className="h5">{message}.</p>
+          <p className="h5" style={p}>
+            {message}.
+          </p>
         </div>
       </div>
     </div>
