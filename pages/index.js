74d4e74996ef9256d99eabd67355ab53fdfc2cbb
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-22 21:58:35 +0900

    328 hello API をページから利用する

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import {useState} from 'react';
 import Layout from '../components/layout';
 import useSWR from 'swr';
 
 export default function Home() {
-  const func = (...args) => fetch(...args).then((res) => res.text());
-  const {data, err} = useSWR('/data.txt', func);
+  const [address, setAddress] = useState('/api/hello');
+  const {data, err} = useSWR(address);
+
+  const onChange = (e) => {
+    setAddress('/api/hello?id=' + e.target.value);
+  };
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data}</h5>
+          <h5 className="mb-4">{JSON.stringify(data)}</h5>
+          <input type="number" className="form-control" onChange={onChange} />
         </div>
       </Layout>
     </div>
