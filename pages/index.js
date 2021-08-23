f8a50656c048f7b47b9b27eb16c597f1b3dc96d2
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-23 09:50:15 +0900

    333 [...params].js を利用する

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,19 @@ import Layout from '../components/layout';
 import useSWR from 'swr';
 
 export default function Home() {
-  const [address, setAddress] = useState('/api/hello');
+  const [pref, setPref] = useState({id: 0, item: 'name'});
+  const [address, setAddress] = useState('/api/hello/' + pref.id + '/' + pref.item);
   const {data, err} = useSWR(address);
 
   const onChange = (e) => {
-    setAddress('/api/hello/' + e.target.value);
+    pref.id = e.target.value;
+    setPref(pref);
+    setAddress('/api/hello/' + pref.id + '/' + pref.item);
+  };
+  const onSelect = (e) => {
+    pref.item = e.target.value;
+    setPref(pref);
+    setAddress('/api/hello/' + pref.id + '/' + pref.item);
   };
 
   return (
@@ -15,7 +23,12 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">{JSON.stringify(data)}</h5>
-          <input type="number" className="form-control" onChange={onChange} />
+          <input type="number" className="form-control form-control-sm mb-2" onChange={onChange} />
+          <select onChange={onSelect} className="form-control form-control-sm">
+            <option value="name">Name</option>
+            <option value="mail">Mail</option>
+            <option value="age">Age</option>
+          </select>
         </div>
       </Layout>
     </div>
