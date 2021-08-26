4db1c85ff01a072c7fd7337e824897d9c5073d87
t-nagaoka-git <dev.t.nagaoka@gmail.com>
2021-08-26 15:09:16 +0900

    404 Google によるログインを利用する

--- pages/fire/index.js
+++ pages/fire/index.js
@@ -3,33 +3,16 @@ import Layout from '../../components/layout';
 import firebase from 'firebase';
 import '../../components/fire';
 
-const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export default function Home() {
-  const mydata = [];
-  const [data, setData] = useState(mydata);
   const [message, setMessage] = useState('wait...');
 
   useEffect(() => {
-    db.collection('mydata')
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((document) => {
-          const doc = document.data();
-          mydata.push(
-            <tr key={document.id}>
-              <td>
-                <a href={'/fire/del?id=' + document.id}>{document.id}</a>
-              </td>
-              <td>{doc.name}</td>
-              <td>{doc.mail}</td>
-              <td>{doc.age}</td>
-            </tr>
-          );
-        });
-        setData(mydata);
-        setMessage('Firebase data.');
-      });
+    auth.signInWithPopup(provider).then((result) => {
+      setMessage('logined: ' + result.user.displayName);
+    });
   }, []);
 
   return (
@@ -37,17 +20,15 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">{message}</h5>
-          <table className="table bg-white text-left">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>{data}</tbody>
-          </table>
+          <p className="h6 text-left">
+            uid: {auth.currentUser != null ? auth.currentUser.uid : ''}
+            <br />
+            displayName: {auth.currentUser != null ? auth.currentUser.displayName : ''}
+            <br />
+            email: {auth.currentUser != null ? auth.currentUser.email : ''}
+            <br />
+            phoneNumber: {auth.currentUser != null ? auth.currentUser.phoneNumber : ''}
+          </p>
         </div>
       </Layout>
     </div>
